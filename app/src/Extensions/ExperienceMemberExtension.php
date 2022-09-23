2e3018601ad33aeacc9238c3e8b35dfd3dad52a7
Steffen Kahl <stevenpaw209@gmail.com>
2022-09-23 18:49:08 +0200

    [CHANGED] Fixed Members

--- app/src/Extensions/ExperienceMemberExtension.php
+++ app/src/Extensions/ExperienceMemberExtension.php
@@ -1,31 +1,25 @@
 <?php
 namespace App\Extensions;
 
-use App\ExperienceDatabase\ExperienceLocation;
 use SilverStripe\ORM\DataExtension;
+use App\ExperienceDatabase\ExperienceLocation;
 
 /**
  * Class \App\Extensions\ExperienceMemberExtension
  *
  * @property \SilverStripe\Security\Member|\App\Extensions\ExperienceMemberExtension $owner
- * @property string $Birthdate
- * @method \SilverStripe\ORM\DataList|\App\ExperienceDatabase\ExperienceLocation[] FavoritePlaces()
+ * @property string $DateOfBirth
+ * @property string $Nickname
+ * @method \SilverStripe\ORM\ManyManyList|\App\ExperienceDatabase\ExperienceLocation[] FavoritePlaces()
  */
 class ExperienceMemberExtension extends DataExtension
 {
     // define additional properties
     private static $db = [
-        "Birthdate" => "Date",
+        'DateOfBirth' => 'Date',
+        'Nickname' => 'Varchar(255)',
     ];
-    private static $has_one = [];
-    private static $has_many = [
+    private static $many_many = [
         "FavoritePlaces" => ExperienceLocation::class,
     ];
-    private static $many_many = [];
-    private static $belongs_many_many = [];
-
-    public function somethingElse()
-    {
-        // You can add any other methods you like, which you can call directly on the member object.
-    }
 }
