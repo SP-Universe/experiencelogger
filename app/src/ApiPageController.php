cd1b8d71359a8c3ca962c7d128b3d0cb116ddec2
Steffen Kahl <stevenpaw209@gmail.com>
2023-09-15 12:38:15 +0200

    [CHANGED Tried to fix image implementation

--- app/src/ApiPageController.php
+++ app/src/ApiPageController.php
@@ -98,8 +98,8 @@ namespace {
                     if ($experience->Description) {
                         $data['items'][$key]['Description'] = $experience->Description;
                     }
-                    if ($experience->PhotoGalleryImages->First->Image->AbsoluteURL) {
-                        $data['items'][$key]['Image'] = $experience->PhotoGalleryImages->First->Image->AbsoluteURL;
+                    if ($experience->PhotoGalleryImages()->first()->Image()) {
+                        $data['items'][$key]['Image'] = $experience->PhotoGalleryImages()->first()->Image()->AbsoluteUrl();
                     }
                     $data['items'][$key]['Location'] = $experience->Parent->Title;
                     $data['items'][$key]['LocationID'] = $experience->ParentID;
