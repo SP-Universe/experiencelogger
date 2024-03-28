beac67e48973e23524c933ab85f7585567864bf1
Steffen Kahl <stevenpaw209@gmail.com>
2024-03-28 20:56:51 +0100

    [CHANGED] Experience Filters Buildup

--- app/client/src/js/pages/ExperienceListPage.js
+++ app/client/src/js/pages/ExperienceListPage.js
@@ -1,5 +1,5 @@
 // Toggle the visibility of filters on Experience List
-const experiencelistfilterToggle = document.querySelector('[data-behaviour="experiencelist_filter"]');
+const experiencelistfilterToggle = document.querySelector('[data-behaviour="experiencelist_togglefilter"]');
 const experiencelistFilters = document.querySelector('[data-behaviour="experiencelist_filters"]');
 if(experiencelistfilterToggle && experiencelistFilters){
     experiencelistfilterToggle.addEventListener('click', function(e) {
@@ -12,22 +12,52 @@ const experiences = document.querySelectorAll('[data-behaviour="experiencecard"]
 let searchExperienceFilters = document.querySelectorAll('[data-behaviour="experiencelist_filter"]');
 if (experiences && searchExperienceFilters) {
     searchExperienceFilters.forEach(filter => {
-        filter.addEventListener('change', function(e) {
-            recalculateExperienceListFilters(experiences, searchExperienceFilters);
-        });
+        if(filter.getAttribute('data-filtertype') === "query"){
+            filter.addEventListener('keyup', function(e) {
+                recalculateExperienceListFilters(experiences, searchExperienceFilters);
+            });
+        } else {
+            filter.addEventListener('change', function(e) {
+                recalculateExperienceListFilters(experiences, searchExperienceFilters);
+            });
+        }
     });
 }
 
-function recalculateExperienceListFilters($experiences, $filters) {
-    const experienceArray = Array.from($experiences);
-    $experiences.forEach(experience => {
+function recalculateExperienceListFilters(experiences, filters) {
+
+    let headlines = document.querySelectorAll('[data-behavior="experience-group-headline"]');
+    if(headlines){
+        headlines.forEach(headline => {
+            headline.remove();
+        });
+    }
+
+    const experienceArray = Array.from(experiences);
+    experiences.forEach(experience => {
         experience.classList.remove('hidebyfilter');
     });
 
-    $filters.forEach(filter => {
+    filters.forEach(filter => {
         const filterType = filter.getAttribute('data-filtertype');
         switch (filterType) {
+            case "query":
+                // Filter by search query
+                experiences.forEach(experience => {
+                    let experienceName = experience.querySelector('.experience_title').textContent.toLowerCase();
+                    let filtervalue = filter.value.toLowerCase();
+                    experienceName = experienceName.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^A-Za-z0-9]/g, '');
+                    filtervalue = filtervalue.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^A-Za-z0-9]/g, '');
+                    if (!experienceName.indexOf(filtervalue) == "") {
+                        if (experienceName.indexOf(filtervalue) != -1) {
+                        } else {
+                            experience.classList.add('hidebyfilter');
+                        }
+                    }
+                });
+                break;
             case "type":
+                // Filter by type
                 experiences.forEach(experience => {
                     const experiencedataJSON = experience.querySelector('.experiencedata');
                     const experiencedata = JSON.parse(experiencedataJSON.textContent);
@@ -41,6 +71,7 @@ function recalculateExperienceListFilters($experiences, $filters) {
                 });
                 break;
             case "state":
+                // Filter by state
                 experiences.forEach(experience => {
                     const experiencedataJSON = experience.querySelector('.experiencedata');
                     const experiencedata = JSON.parse(experiencedataJSON.textContent);
@@ -53,6 +84,7 @@ function recalculateExperienceListFilters($experiences, $filters) {
                     }
                 });
             case "sort":
+                // Sort by distance, title or type
                 if(filter.value == "distance"){
                     experienceArray.sort(function(a, b) {
                         return a.getAttribute('data-distance') - b.getAttribute('data-distance');
@@ -80,28 +112,6 @@ function recalculateExperienceListFilters($experiences, $filters) {
                 }
             default:
                 break;
-        }
-    })
-}
-
-
-// Searchbar for Experiences
-const searchExperienceBar = document.querySelector('#search-experience');
-if(searchExperienceBar) {
-    searchExperienceBar.addEventListener('keyup', searchExperience);
-}
-
-function searchExperience(e) {
-    const searchValue = e.target.value.toLowerCase();
-    const experiences = document.querySelectorAll('.experience_card');
-    experiences.forEach(experience => {
-        const experienceName = experience.querySelector('.experience_title').textContent.toLowerCase();
-        if (experienceName.indexOf(searchValue) != -1) {
-            experience.classList.add('show');
-            experience.classList.remove('hide');
-        } else {
-            experience.classList.add('hide');
-            experience.classList.remove('show');
         }
     })
 }
--- app/templates/App/Overview/Layout/LocationPage_location.ss
+++ app/templates/App/Overview/Layout/LocationPage_location.ss
@@ -130,8 +130,8 @@
                 <h2>Experiences</h2>
                 <div class="location_experiences">
                     <div class="search_bar" id="search-experience-bar">
-                        <input type="text" name="search" id="search-experience" placeholder="Search for an experience" />
-                        <a class="experience_togglefilter" data-behaviour="experiencelist_filter">
+                        <input type="text" name="search" id="search-experience" placeholder="Search for an experience" data-behaviour="experiencelist_filter" data-filtertype="query"/>
+                        <a class="experience_togglefilter" data-behaviour="experiencelist_togglefilter">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 48 48"><path fill="currentColor" d="M22 40q-.85 0-1.425-.575Q20 38.85 20 38V26L8.05 10.75q-.7-.85-.2-1.8Q8.35 8 9.4 8h29.2q1.05 0 1.55.95t-.2 1.8L28 26v12q0 .85-.575 1.425Q26.85 40 26 40Z"/></svg>
                         </a>
                         <a class="experience_activatelocation" data-behaviour="locationTracker">
@@ -159,7 +159,7 @@
                     </div>
                     <div class="experience_list">
                         <% loop $GroupedExperiencesByState %>
-                            <div class="state_hl">
+                            <div class="state_hl" data-behavior="experience-group-headline" data-state="$Children.First.State">
                                 <h2>$Children.First.State</h2>
                             </div>
                             <% loop $Children %>
