a36f88d6285f8777ddd66515f602738e15651a23
Steffen Kahl <stevenpaw209@gmail.com>
2024-05-21 02:11:15 +0200

    [CHANGED] Header to be fixed

--- app/client/src/js/tools/CardEffect.js
+++ app/client/src/js/tools/CardEffect.js
@@ -1,47 +1,50 @@
 const $card = document.querySelector('[data-behavior="card"]');
-let bounds;
 
-console.log($card);
+if($card) {
+    let bounds;
 
-function rotateToMouse(e) {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  const leftX = mouseX - bounds.x;
-  const topY = mouseY - bounds.y;
-  const center = {
-    x: leftX - bounds.width / 2,
-    y: topY - bounds.height / 2
-  }
-  const distance = Math.sqrt(center.x**2 + center.y**2);
+    console.log($card);
 
-  $card.style.transform = `
-    scale3d(1.0, 1.0, 1.0)
-    rotate3d(
-      ${center.y / 100},
-      ${-center.x / 100},
-      0,
-      ${Math.log(distance)* 2}deg
-    )
-  `;
+    function rotateToMouse(e) {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const leftX = mouseX - bounds.x;
+    const topY = mouseY - bounds.y;
+    const center = {
+        x: leftX - bounds.width / 2,
+        y: topY - bounds.height / 2
+    }
+    const distance = Math.sqrt(center.x**2 + center.y**2);
 
-  $card.querySelector('.glow').style.backgroundImage = `
-    radial-gradient(
-      circle at
-      ${center.x * 2 + bounds.width/2}px
-      ${center.y * 2 + bounds.height/2}px,
-      #ffffff55,
-      #0000000f
-    )
-  `;
+    $card.style.transform = `
+        scale3d(1.0, 1.0, 1.0)
+        rotate3d(
+        ${center.y / 100},
+        ${-center.x / 100},
+        0,
+        ${Math.log(distance)* 2}deg
+        )
+    `;
+
+    $card.querySelector('.glow').style.backgroundImage = `
+        radial-gradient(
+        circle at
+        ${center.x * 2 + bounds.width/2}px
+        ${center.y * 2 + bounds.height/2}px,
+        #ffffff55,
+        #0000000f
+        )
+    `;
+    }
+
+    $card.addEventListener('mouseenter', () => {
+    bounds = $card.getBoundingClientRect();
+    document.addEventListener('mousemove', rotateToMouse);
+    });
+
+    $card.addEventListener('mouseleave', () => {
+    document.removeEventListener('mousemove', rotateToMouse);
+    $card.style.transform = '';
+    $card.style.background = '';
+    });
 }
-
-$card.addEventListener('mouseenter', () => {
-  bounds = $card.getBoundingClientRect();
-  document.addEventListener('mousemove', rotateToMouse);
-});
-
-$card.addEventListener('mouseleave', () => {
-  document.removeEventListener('mousemove', rotateToMouse);
-  $card.style.transform = '';
-  $card.style.background = '';
-});
