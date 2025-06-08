0c70e13b12f286675ef389934c4fe8ccfe522089
ZwibblTV <zwibbltv@gmail.com>
2025-06-08 16:10:14 +0200

    [~] removed TinyMCEConfig

--- vite.config.js
+++ vite.config.js
@@ -17,23 +17,23 @@ export default defineConfig(({command}) => {
     },
     // base: (command === 'build') ? '/_resources/app/client/dist/' : '/', // TODO: .env variable, only on build
     base: './',
-    publicDir: 'app/client/public',
-    build: {
-        // cssCodeSplit: false,
-        outDir: './app/client/dist',
-        manifest: true,
-        sourcemap: true,
-        rollupOptions: {
-            input: {
-                'main.js': './app/client/src/js/main.js',
-                'main.scss': './app/client/src/scss/main.scss',
-                'editor.scss': './app/client/src/scss/editor.scss',
+        publicDir: 'app/client/public',
+        build: {
+          // cssCodeSplit: false,
+            outDir: './app/client/dist',
+            manifest: true,
+            sourcemap: true,
+            rollupOptions: {
+                input: {
+                    'main.js': './app/client/src/js/main.js',
+                    'main.scss': './app/client/src/scss/main.scss',
+                    'editor.scss': './app/client/src/scss/editor.scss',
                 },
+            },
         },
-    },
-    css: {
-        devSourcemap: true,
-    },
-    plugins: [],
-  }
+        plugins: [],
+        css: {
+            devSourcemap: true,
+        }
+    }
 })
